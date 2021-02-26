e77269ad81ec537e513d438545ae7966b6f3371a
taisuke-salty <taisuke.1018.p@gmail.com>
2021-02-26 23:40:05 +0900

    time_zone Tokyo

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module TenkiApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
   end
 end
 
-config.time_zone = 'Tokyo'
+
