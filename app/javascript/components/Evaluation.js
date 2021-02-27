f3d89b4baca828c82f79b934de00886de5a1f654
taisuke-salty <taisuke.1018.p@gmail.com>
2021-02-28 01:13:02 +0900

    evaluation component cannot visible

---
+++ app/javascript/components/Evaluation.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Evaluation extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+          <div classname="evaluation-img-area" >
+            <img src='happy1@2x.png' classname="evaluation-img" />
+          </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Evaluation
