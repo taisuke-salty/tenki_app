4026804e4c848905cd7835e01e620b667deb5486
taisuke-salty <taisuke.1018.p@gmail.com>
2021-04-14 20:51:35 +0900

    redux stop

--- app/javascript/reduxPractice/counter/index.js
+++ app/javascript/reduxPractice/counter/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import App from './components'
 import { Provider } from 'react-redux'
 import store from '../store'
+import counter from "./containers/counter"
 
 export default class Counter extends Component {
   render () {
@@ -12,6 +13,7 @@ export default class Counter extends Component {
     )
   }
 }
+
 
 
 
