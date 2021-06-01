b48283b4e75a4665a1d7fd49f488a1c7ae446107
taisuke-salty <taisuke.1018.p@gmail.com>
2021-06-01 21:48:26 +0900

    devise japanese

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module TenkiApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+
+    # deviseの日本語化https://pikawaka.com/rails/devise
+    config.i18n.default_locale = :ja
   end
 end
 
