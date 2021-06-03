ba9fa2493020d5fbd8da6240aa324eb35734e6a7
taisuke-salty <taisuke.1018.p@gmail.com>
2021-06-04 00:21:47 +0900

    exception record_not_found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
     conditions_index_path
   end
 
+  # 白本p 392  recordが見つからないときの例外処理
+  rescue_from ActiveRecord::RecordNotFound, with: :id_invalid
+
 private
     def sign_in_required
         redirect_to new_user_session_url unless user_signed_in?
+    end
+
+    def id_invalid(e)
+      render 'shared/record_not_found', status: 404
     end
 
 end
