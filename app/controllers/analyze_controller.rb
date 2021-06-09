e98a270e290993ab6abc8985e33245e21652a4f9
taisuke-salty <taisuke.1018.p@gmail.com>
2021-06-09 19:43:46 +0900

    edit view a_day.html.erb

--- app/controllers/analyze_controller.rb
+++ app/controllers/analyze_controller.rb
@@ -4,29 +4,22 @@ class AnalyzeController < ApplicationController
 
     @a_day = params[:x]
     set_cards
-    # @conditions = Condition.where(evaluate_on: @a_day)
-    # @cards = Card.where(feel_on: @a_day.in_time_zone.all_day)
 
-    # https://techracho.bpsinc.jp/hachi8833/2016_08_19/24876
   end
 
   def today_day
     @a_day = Date.current
     set_cards
     render("analyze/a_day")
-    # d= Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-    # @conditions = Condition.where(evaluate_on: d)
-  # binding.pry
-    # @cards = Card.where(feel_on: d.in_time_zone.all_day)
-    # https://techracho.bpsinc.jp/hachi8833/2016_08_19/24876
+
   end
 
   private
     def set_cards
       # binding.pry
-
-      @conditions = Condition.where(evaluate_on: @a_day)
-      @cards = Card.where(feel_on: @a_day.in_time_zone.all_day)
+      @conditions = Condition.where(evaluate_on: @a_day, user_id: current_user.id)
+      @cards = Card.where(feel_on: @a_day.in_time_zone.all_day, user_id: current_user.id)
+          # https://techracho.bpsinc.jp/hachi8833/2016_08_19/24876
     end
 
 
