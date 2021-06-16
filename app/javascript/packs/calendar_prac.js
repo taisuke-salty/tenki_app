4ae90998adcc421889e85d34997067faab8831f1
taisuke-salty <taisuke.1018.p@gmail.com>
2021-06-16 23:51:04 +0900

    calendar prac made simple calendar

--- app/javascript/packs/calendar_prac.js
+++ app/javascript/packs/calendar_prac.js
@@ -1,40 +1,47 @@
+// JSの練習を兼ねたカレンダー描画
+// Application.html.erbにid=calendar-pracのdiv要素を足し、それのHTML要素を書き換えるJSを書く。
+// https://qiita.com/kan_dai/items/b1850750b883f83b9bee
+
 const weeks = ['日', '月', '火', '水', '木', '金', '土']
-const date = new Date()
-const year = date.getFullYear()
-const month = date.getMonth() + 1
-const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
-const endDate = new Date(year, month,  0) // 月の最後の日を取得
-const endDayCount = endDate.getDate() // 月の末日
-const startDay = startDate.getDay() // 月の最初の日の曜日を取得
-let dayCount = 1 // 日にちのカウント
-let calendarHtml = '' // HTMLを組み立てる変数
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const startDate = new Date(year, month -1 , 1);
+const endDate = new Date(year, month, 0);
+const startDay = startDate.getDay();
+const endDateCount = endDate.getDate();
+let dateCount = 1
 
-calendarHtml += '<h1>' + year  + '/' + month + '</h1>'
-calendarHtml += '<table>'
+let calendarHtml = "";
+calendarHtml += "<h1>" + year + "/" + month + "</h1>";
 
-// 曜日の行を作成
-for (let i = 0; i < weeks.length; i++) {
-    calendarHtml += '<td>' + weeks[i] + '</td>'
-}
+calendarHtml += "<table>";
 
-for (let w = 0; w < 6; w++) {
-    calendarHtml += '<tr>'
-
-    for (let d = 0; d < 7; d++) {
-        if (w == 0 && d < startDay) {
-            // 1行目で1日の曜日の前
-            calendarHtml += '<td></td>'
-        } else if (dayCount > endDayCount) {
-            // 末尾の日数を超えた
-            calendarHtml += '<td></td>'
-        } else {
-            calendarHtml += '<td>' + dayCount + '</td>'
-            dayCount++
-        }
+// 曜日行
+  calendarHtml += "<tr>" ;
+    for(let i = 0; i < 7; i++) {
+      calendarHtml += "<td>" + weeks[i] + "</td>" ;
     }
-    calendarHtml += '</tr>'
-}
-calendarHtml += '</table>'
+  calendarHtml += "</tr>" ;
+
+// 日にち行
+  for(let w = 0; w < 6; w++) {
+    calendarHtml += "<tr>" ;
+    for(let d = 0; d < 7; d++){
+      if(w == 0 && d < startDay){
+        calendarHtml += "<td></td>";
+      } else if(dateCount > endDateCount){
+        calendarHtml += "<td></td>";
+      } else {
+        calendarHtml += "<td>" + dateCount +  "</td>";
+        dateCount ++ ;
+      }
+    }
+
+    calendarHtml += "</tr>" ;
+  }
+
+calendarHtml += "</table>" ;
 
 var renderCalender = () => {document.querySelector('#calendar-prac').innerHTML = calendarHtml}
 
