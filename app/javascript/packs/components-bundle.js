7af3ad13fb7d267b486cac47c89fdba98ea928ba
taisuke-salty <taisuke.1018.p@gmail.com>
2021-07-13 19:30:12 +0900

    check require components from any dir

--- app/javascript/packs/components-bundle.js
+++ app/javascript/packs/components-bundle.js
@@ -1,7 +1,7 @@
 import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
-import Evaluation from '../bundles/HelloWorld/components/Evaluation';
+import Evaluation from '../components/Evaluation';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
